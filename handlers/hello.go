486816700e8653a7129cd49de5f21759f6c6da33
Ni Xin <nxin@Nis-MacBook-Air.local>
2022-06-20 09:30:02 -0700

    git tutorial

---
+++ handlers/hello.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+// name of the type
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+
+	h.l.Println("Hello World")
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("data %s\n =", d)
+
+	fmt.Fprintf(rw, "Hello %s", d)
+}
